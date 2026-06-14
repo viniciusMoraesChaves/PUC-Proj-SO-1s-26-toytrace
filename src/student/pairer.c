bb18ad5e8388577dc43f17e53d2babfe457a2dbf
Guilherme Dias Cavalheri <gui.dias.c12@gmail.com>
2026-06-14 12:52:29 -0300

    Versão final : garantindo que não haja multiplas entradas com pairer->entry
    Impedi que houvessse 2 entradas em que o evento já é de entrada e o pairer->has_entrey ja ser 1 tambem, impeidndo que haja essa interação.

--- src/student/pairer.c
+++ src/student/pairer.c
@@ -3,13 +3,15 @@
 #include <stdio.h>
 #include <sys/syscall.h>
 #include <trace_helpers.h>
-#include <stdint.h>
 
 int student_pair_syscall(struct syscall_pairer *pairer,
                          const struct syscall_event *ev,
                          struct syscall_event *out)
 {
-   if (ev->entering) {
+    if (ev->entering) {
+        if(pairer->has_entry){
+            return -1;
+        }
     pairer->entry = *ev;
     pairer->has_entry = 1;
 
